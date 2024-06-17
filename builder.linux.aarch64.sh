9b250c7c1d68bd3da2362d196af993e933ada33f
pan_ding_rong <panseng.dr@qq.com>
2024-06-17 10:39:26 +0800

    build: 调整编译脚本

--- builder.linux.aarch64.sh
+++ builder.linux.aarch64.sh
@@ -6,6 +6,6 @@ export GOARCH=arm64
 
 # 通过 在根目录 make build
 # 获取 编译的信息
-go build "-trimpath" -ldflags="-X "github.com/gocnpan/kubo".CurrentCommit=v0290" -o "cmd/ipfs/ipfs" "github.com/gocnpan/kubo/cmd/ipfs"
+go build "-trimpath" -ldflags="-X "github.com/gocnpan/kubo".CurrentCommit=v0290" -o "cmd/ipfs/ipfs.aarch64" "github.com/gocnpan/kubo/cmd/ipfs"
 
 # mv cmd/ipfs/ipfs ./
